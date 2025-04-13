bf43c6a3c4cd9f16eebe4badd7849f7ed67c4e8a
hiro84hassy <hiro84hassy@gmail.com>
2025-04-13 10:32:22 +0900

    Googleログイン連携とFireStore保存対応

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwsG1zm7SkUUdvc7I8_BPAgpLP6NO-Ihk",
+  authDomain: "task-manager-add7a.firebaseapp.com",
+  projectId: "task-manager-add7a",
+  storageBucket: "task-manager-add7a.appspot.com",
+  messagingSenderId: "469620876083",
+  appId: "1:469620876083:web:7464f25165894864129036",
+  measurementId: "G-GT9WDXHF7T"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
